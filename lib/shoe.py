5f29f2e3e976efe80c8ab2b97685d8dc11ec75b5
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-03-16 22:18:36 -0400

    passed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+import ipdb
+
 
 class Shoe:
-    pass
+
+    def __init__(self, brand, color="white", size= 0):
+
+        self.brand = brand
+        self.color = color
+        self.size = size
+
+    def cobble(self):
+        self.condition = "New"
+        print ("Your shoe is as good as new!")
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, value):
+        if type(value) == int:
+            self._size = value
+
+        print("size must be an integer")
+     
+    size = property(get_size, set_size)
